9893a2dd3a8a485caacd4a8e54eae6e3ee95429d
Chris Tippett <git@christippett.dev>
2024-07-31 10:41:02 +0100

    Add logging

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 import datetime
 import hashlib
+import logging
 
 import requests
 from environs import Env
 from flask import Flask, Response, request
 from icalendar import Calendar, Event
 
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
 env = Env()
 env.read_env()
+
 
 PORKBUN_API_SECRET = env.str("PORKBUN_API_SECRET", default=None)
 PORKBUN_API_KEY = env.str("PORKBUN_API_KEY", default=None)
@@ -20,12 +25,24 @@ def calendar():
     # Allow API credentials to be provided via Basic Auth
     auth = request.authorization
     if auth:
+        log.info(auth)
         api_key = auth.username
         api_secret = auth.password
     else:
         api_key = PORKBUN_API_KEY
         api_secret = PORKBUN_API_SECRET
-    domains = list_domains(api_key, api_secret)
+
+    if not api_key or not api_secret:
+        headers = {"WWW-Authenticate": 'Basic realm="Login Required"'}
+        return Response("Missing Porkbun API credentials", status=401, headers=headers)
+
+    try:
+        domains = list_domains(api_key, api_secret)
+        log.info(f"{len(domains)} domains found")
+    except requests.HTTPError as e:
+        # Mirror whatever error returned by the API
+        log.error(f"Porkbun API error: {e.response.text}")
+        return Response(str(e), status=e.response.status_code)
     response_text = generate_icalendar(domains)
     response = Response(response_text, content_type="text/calendar")
     response.headers["Content-Disposition"] = "attachment; filename=calendar.ics"
@@ -33,8 +50,6 @@ def calendar():
 
 
 def list_domains(key: str | None, secret: str | None):
-    if not key or not secret:
-        raise ValueError("Missing Porkbun API key/secret")
     api_base = "https://api.porkbun.com/api/json/v3"
     body = {
         "secretapikey": secret,
